c22d961d76bbaf735b10f7451803847beb3367ef
Thomas (Junrui) Hu <junruihu@gmail.com>
2016-10-05 13:27:26 +0900

    improve tail recursive functions using defaults for keyword args

--- integer sequences.py
+++ integer sequences.py
@@ -6,14 +6,11 @@ def fibonacci_r(n):
         return fibonacci_r(n - 1) + fibonacci_r(n - 1)
 
 
-def fibonacci_tr(n):
-    def loop(n, a, b):
-        if not n:
-            return a
-        else:
-            return loop(n - 1, b, a + b)
-
-    return loop(n, 0, 1)
+def fibonacci_tr(n, a=0, b=1):
+    if not n:
+        return a
+    else:
+        return fibonacci_tr(n - 1, b, a + b)
 
 
 def fibonacci_it(n):
@@ -26,15 +23,34 @@ def fibonacci_it(n):
     return a
 
 
-def sum_to_n(n):
+def sum_to_n_it(n):
+    return sum(range(n + 1))
+
+
+def sum_to_n_r(n):
+    if not n:
+        return 0
+    else:
+        return n + sum_to_n_r(n - 1)
+
+
+def sum_to_n_tr(n, acc=0):
+    if not n:
+        return acc
+    else:
+        return sum_to_n_tr(n - 1, acc + n)
+
+
+def sum_to_n_f(n):
     return n * (n + 1) // 2
 
 
 def seriesa(start, step, steps):
     # finite arthimetic series
-    return start + sum_to_n(step) * step
+    return start + sum_to_n_f(step) * step
 
 
 def seriesg(start, ratio, steps):
     # finite geometric series
     return start * ratio ** (steps + 1) // (ratio - 1)
+
