3105266c48bb36250db77514012d2c2235b4e108
Thomas (Junrui) Hu <junruihu@gmail.com>
2016-10-05 22:39:18 +0900

    insertion sort, quick sort

--- sorting.py
+++ sorting.py
@@ -14,6 +14,7 @@ def sort_bubble(xs):
                 in_order = False
                 xs[j], xs[j + 1] = xs[j + 1], xs[j]
     return xs
+# print(sort_bubble([5, 4, 3, 2, 1]))
 
 
 def sort_selection(xs):
@@ -26,6 +27,7 @@ def sort_selection(xs):
                 xs[i], xs[j] = y, x
 
     return xs
+# print(sort_selection([1, 4, 5, 2, 188, 9]))
 
 
 def sort_insertion(xs):
@@ -33,17 +35,69 @@ def sort_insertion(xs):
 
     for i in range(1, n):
         j = i - 1
-        while xs[i] < xs[j] and j:
+        while xs[i] < xs[j] and j >= 0:
             j -= 1
 
-        if xs[i] < xs[j]:
-            xs[i], xs[j] = xs[j], xs[i]
+        if xs[i] < xs[j + 1]:  # out of order
+            temp = xs[i]
+            k = i
+            while k > j:
+                xs[k] = xs[k-1]
+                k -= 1
 
+            xs[j + 1] = temp
+        print(xs)
     return xs
+#print(sort_insertion([1, 5, 3, 6, 2]))
 
 
-print(sort_insertion([1, 5, 3, 6, 2]))
+def merge(xs, ys):
+    INF = float('inf')
+    xs.append(INF)
+    ys.append(INF)
+    i, j = 0, 0
+    merged = []
+    for k in range(len(xs) + len(ys) - 2):
+        # print(merged)
+        if xs[i] < ys[j]:
+            merged.append(xs[i])
+            i += 1
+        else:
+            merged.append(ys[j])
+            j += 1
+    return merged
 
-#print(sort_selection([1, 4, 5, 2, 188, 9]))
 
-#print(sort_bubble([5, 4, 3, 2, 1]))
+def sort_merge(xs):
+    if len(xs) < 2:
+        return xs
+
+    mid = len(xs) // 2
+    left, right = xs[:mid], xs[mid:]  # second half is bigger if odd
+    return merge(sort_merge(left), sort_merge(right))
+# print(sort_merge([1, 5, 3, 6, 2]))
+
+
+def sort_quick(xs):
+    # choose pivot to be median of first, middle, and last
+    n = len(xs)
+    if n <= 1:
+        return xs
+    else:
+        pivot = sorted([xs[0], xs[-1], xs[n // 2]])[1]
+
+        end_left = 0
+        start_right = n - 1
+        for i in range(n):
+            if xs[i] < pivot:
+                if i > end_left:  # LLRL -> LLLR
+                    xs[i], xs[end_left] = xs[end_left], xs[i]
+                end_left += 1
+            elif xs[i] > pivot:  # greater than pivot
+                if i < start_right:
+                    xs[i], xs[start_right] = xs[start_right], xs[i]
+                start_right -= 1
+
+        print(xs, pivot)
+        return sort_quick(xs[:end_left]) + sort_quick(xs[end_left:])
+print(sort_quick([1, 5, 3, 6, 2]))
