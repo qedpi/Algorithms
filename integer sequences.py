7f765c3f124f30bae034bdbcce305a750bf3f155
Thomas (Junrui) Hu <junruihu@gmail.com>
2016-10-05 13:39:48 +0900

    require kwargs for tailrecursive accumulaters

--- integer sequences.py
+++ integer sequences.py
@@ -1,3 +1,6 @@
+from functools import reduce
+from operator import add
+
 
 def fibonacci_r(n):
     if n <= 2:
@@ -6,11 +9,11 @@ def fibonacci_r(n):
         return fibonacci_r(n - 1) + fibonacci_r(n - 1)
 
 
-def fibonacci_tr(n, a=0, b=1):
+def fibonacci_tr(n, *, a=0, b=1):
     if not n:
         return a
     else:
-        return fibonacci_tr(n - 1, b, a + b)
+        return fibonacci_tr(n - 1, a=b, b=a + b)
 
 
 def fibonacci_it(n):
@@ -34,23 +37,31 @@ def sum_to_n_r(n):
         return n + sum_to_n_r(n - 1)
 
 
-def sum_to_n_tr(n, acc=0):
+def sum_to_n_tr(n, *, acc=0):
     if not n:
         return acc
     else:
-        return sum_to_n_tr(n - 1, acc + n)
+        return sum_to_n_tr(n - 1, acc=acc + n)
 
 
-def sum_to_n_f(n):
+def sum_to_n_e(n):
     return n * (n + 1) // 2
 
 
-def seriesa(start, step, steps):
+def sum_to_n_f(n):
+    return reduce(add, range(n + 1))
+
+
+def sum_to_n_b(n):
+    return sum(range(n + 1))
+
+
+def series_arith(start, step, steps):
     # finite arthimetic series
-    return start + sum_to_n_f(step) * step
+    return start + sum_to_n_e(step) * step
 
 
-def seriesg(start, ratio, steps):
+def series_geomet(start, ratio, steps):
     # finite geometric series
     return start * ratio ** (steps + 1) // (ratio - 1)
 
