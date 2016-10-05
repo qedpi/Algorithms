31e5ec3c3dc1dc1b3e769fea414c1e36a4abb730
Thomas (Junrui) Hu <junruihu@gmail.com>
2016-10-05 13:18:46 +0900

    + fibonacci nontailrecursive, iterative, tail recursive

--- integer sequences.py
+++ integer sequences.py
@@ -1,6 +1,12 @@
 
+def fibonacci_r(n):
+    if n <= 2:
+        return 1
+    else:
+        return fibonacci_r(n - 1) + fibonacci_r(n - 1)
 
-def fibonacci(n):
+
+def fibonacci_tr(n):
     def loop(n, a, b):
         if not n:
             return a
@@ -8,5 +14,16 @@ def fibonacci(n):
             return loop(n - 1, b, a + b)
 
     return loop(n, 0, 1)
+
+
+def fibonacci_it(n):
+    a, b = 0, 1
+
+    while n:
+        n -= 1
+        a, b = b, a + b
+
+    return a
+
 
 
